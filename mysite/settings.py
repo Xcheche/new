592f82f68e16ee7ee7fbc840903214039f8bafa8
xcheche <chekwubechukwu23@gmail.com>
2024-07-28 11:38:31 +0100

    added login/logout feature

--- mysite/settings.py
+++ mysite/settings.py
@@ -133,3 +133,4 @@ MEDIA_URL = "/media/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+LOGIN_REDIRECT_URL = "food:index"
--- users/views.py
+++ users/views.py
@@ -1,25 +1,28 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import
+#  UserCreationForm using register form from from.py but inheriting usercreationform
 from django.contrib import messages
+
+from .forms import RegisterForm
 
 # Create your views here.
 
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get("username")
             messages.success(
-                request, f"Account successfully created. Welcome {username}!"
+                request, f" successfully created a new account. Login Below❤️ {username}!"
             )
-            return redirect("food:index")
+            return redirect("users:login")
         else:
             messages.error(
                 request, "Registration failed. Please correct the errors below."
             )
     else:
-        form = UserCreationForm()
+        form = RegisterForm()
 
     return render(request, "users/register.html", {"form": form})
