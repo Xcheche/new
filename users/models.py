4fe1695e31a8f559ee9c88dd2001eae4511b362a
xcheche <chekwubechukwu23@gmail.com>
2024-08-07 09:19:58 +0100

    added django signal

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    full_name = models.CharField(max_length=70,help_text="Enter your full name",default="ok")
+    full_name = models.CharField(max_length=70,help_text="Enter your full name")
     location = models.CharField(max_length=200)
     image = models.ImageField(default='profilepics/profilepic.png', upload_to='profilepics')
     
---
+++ users/signals.py
@@ -0,0 +1,28 @@
+# Importing the 'post_save' signal from Django's 'db.models' module
+from django.db.models.signals import post_save
+
+# Importing the built-in User model from Django's 'contrib.auth.models' module
+from django.contrib.auth.models import User
+
+# Importing the 'receiver' decorator from Django's 'dispatch' module
+from django.dispatch import receiver
+
+# Importing the Profile model from the current application's 'models' module
+from .models import Profile
+
+
+# This decorator registers the 'create_profile' function as a receiver for the 'post_save' signal
+# sent by the User model. This means the 'create_profile' function will be called every time a User is saved.
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    # If a new User instance is created (not just updated), create a corresponding Profile instance.
+    if created:
+        Profile.objects.create(user=instance)
+
+# This decorator registers the 'save_profile' function as a receiver for the 'post_save' signal
+# sent by the User model. This means the 'save_profile' function will be called every time a User is saved.
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    # Save the associated Profile instance whenever the User instance is saved.
+    instance.profile.save()
+
